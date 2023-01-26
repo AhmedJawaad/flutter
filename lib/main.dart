8e6a7cf0cd6279edd36184d2a84ab060f74258d9
Ahmed <ahmed887463@gmail.com>
2023-01-26 08:05:30 +0000

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'LoginScreen.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: LoginScreen(),
+      ),
+    );
+  }
+}
