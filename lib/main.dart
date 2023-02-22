daf36c2840747236a61079a04080193bc132fe12
Ahmed <ahmed887463@gmail.com>
2023-02-22 10:24:17 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:learn/stateapp.dart';
+
+import 'MassengerScreenList.dart';
+
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:StateApp(),
+    ),
+
+  );
+}
